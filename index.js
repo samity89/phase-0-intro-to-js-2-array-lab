cc27677ce2127a8552fb07d569dca95e872957ae
Samuel Amity <sam.amity@gmail.com>
2022-06-14 18:54:08 -0400

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(){
+    const copyOfCats = [...cats, "Broom"];
+    return copyOfCats;
+}
+
+function prependCat(){
+    const newCats = ["Arnold", ...cats];
+    return newCats;
+}
+
+function removeLastCat(){
+    const lessLastCat = cats.slice(0, -1);
+    return lessLastCat;
+}
+
+function removeFirstCat(){
+    const lessFirstCat = cats.slice(1);
+    return lessFirstCat;
+}
